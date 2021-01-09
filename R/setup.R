422b08cf3529653260413cf846a4edd586ad6bb7
Jameel Alsalam <alsalam.jameel@epa.gov>
2021-01-09 05:58:23 -0500

    export pyam, newer reticulate setup

--- R/setup.R
+++ R/setup.R
@@ -1,9 +1,13 @@
 # global reference to pyam (will be initialized in .onLoad)
+
+#' @export
 pyam <- NULL
 
 .onLoad <- function(libname, pkgname) {
+  reticulate::configure_environment(pkgname)
+
   # use superassignment to update global reference to pyan
-  pyam <<- reticulate::import("pyam", delay_load = TRUE)
+  pyam <<- reticulate::import("pyam", delay_load = TRUE, convert = FALSE)
 }
 
 have_pyam <- function() {
